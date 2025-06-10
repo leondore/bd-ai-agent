a8045daeab851b8d33205e3e62090546b289fc21
Leon Dore <leon.dore@gmail.com>
2025-06-10 13:49:53 -0400

    add verbose flag

--- main.py
+++ main.py
@@ -9,15 +9,23 @@ if len(sys.argv) < 2:
     print("Usage: python main.py <prompt>")
     sys.exit(1)
 
+verbose = False
 prompt = sys.argv[1]
+if len(sys.argv) > 2:
+    verbose = sys.argv[2] == "--verbose"
 
 client = genai.Client(api_key=api_key)
 
+messages = [genai.types.Content(role="user", parts=[genai.types.Part(text=prompt)])]
+
 response = client.models.generate_content(
     model="gemini-2.0-flash-001",
-    contents=prompt,
+    contents=messages,
 )
 
 print(response.text)
-print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-print("Response tokens:", response.usage_metadata.candidates_token_count)
+
+if verbose:
+    print("User prompt:", prompt)
+    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+    print("Response tokens:", response.usage_metadata.candidates_token_count)
